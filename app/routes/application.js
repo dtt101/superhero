31bb96835aec44560402b9bb5805b16780eded2e
David Thompson <dtt101@gmail.com>
2015-03-23 09:31:46 +0000

    Test with github API

---
+++ app/routes/application.js
@@ -0,0 +1,9 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+  model: function() {
+    return Ember.$.getJSON('https://api.github.com/repos/emberjs/ember.js/pulls').then(function(data) {
+      return data.splice(0, 3);
+    });
+  }
+});

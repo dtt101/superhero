ef0ccab5c093b5bcf3e12944748105164df5bc14
David Thompson <dtt101@gmail.com>
2015-04-04 09:21:10 +0100

    Add resource URI to character model

--- app/models/character.js
+++ app/models/character.js
@@ -3,5 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   description: DS.attr('string'),
-  modified: DS.attr('date')
+  modified: DS.attr('date'),
+  resourceURI: DS.attr('string')
 });

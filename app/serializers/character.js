0071d6efbdccc37180c3da295ed2d7b7a952829d
David Thompson <dtt101@gmail.com>
2015-04-05 08:30:06 +0100

    Handle metdata in serializer

--- app/serializers/character.js
+++ app/serializers/character.js
@@ -3,8 +3,10 @@ import DS from 'ember-data';
 export default DS.RESTSerializer.extend({
 
   extract: function(store, type, payload, id, requestType) {
-    var characters = {};
-    characters['characters'] = payload.data.results;
-    return this._super(store, type, characters, id, requestType);
+    var results = {};
+    results['characters'] = payload.data.results;
+    results['meta'] = payload.data;
+    delete results.meta.results;
+    return this._super(store, type, results, id, requestType);
   }
 });

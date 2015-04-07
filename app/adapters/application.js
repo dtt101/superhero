3fe8d7056bb757026196fc8c488135c77c211bc7
David Thompson <dtt101@gmail.com>
2015-04-07 14:55:19 +0100

    Removed commented code

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -7,9 +7,6 @@ export default DS.RESTAdapter.extend({
   buildURL: function(type, id, snapshot) {
     return this._super(type, id, snapshot) + this.generateSignature();
   },
-  // findHasMany: function(store, snapshot, url){
-  //   return this._super(store, snapshot, url + this.generateSignature());
-  // },
   generateSignature: function(){
     var ts = new Date().valueOf();
     var publicKey = ENV.MARVEL_PUBLIC_KEY;

cd47fbd7aef859e1ae6c23acb2ff3f7a76bba69d
David Thompson <dtt101@gmail.com>
2015-04-26 11:23:40 +0100

    test for thumbnail transform

--- tests/unit/transforms/thumbnail-test.js
+++ tests/unit/transforms/thumbnail-test.js
@@ -3,13 +3,18 @@ import {
   test
 } from 'ember-qunit';
 
-moduleFor('transform:thumbnail', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
-});
+moduleFor('transform:thumbnail', {});
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('It transforms the provided object into a single URL', function(assert) {
   var transform = this.subject();
-  assert.ok(transform);
+  var obj = {
+    path: 'http://i.annihil.us/u/prod/marvel/i/mg/6/20/52602f21f29ec',
+    extension: 'jpg'
+  };
+  var uri = transform.deserialize(obj);
+  assert.equal(
+    uri,
+    'http://i.annihil.us/u/prod/marvel/i/mg/6/20/52602f21f29ec.jpg',
+    'Thumbnail returns a single URL'
+  );
 });

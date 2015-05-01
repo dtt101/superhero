a74ceb479515f3a4328c9877e18840da8fdbf8ef
David Thompson <dtt101@gmail.com>
2015-05-01 19:19:24 +0100

    Generate signature adapter test

--- tests/unit/adapters/application-test.js
+++ tests/unit/adapters/application-test.js
@@ -7,11 +7,20 @@ moduleFor('adapter:application', 'ApplicationAdapter', {});
 
 test('Adapter builds the correct URL', function(assert) {
   var adapter = this.subject();
-  assert.ok(adapter);
   var pattern = /^\/v1\/public\/characters\?ts=\d+\&apikey=.*$/;
   assert.equal(
     pattern.test(adapter.buildURL('character')),
     true,
     "buildURL should create a valid URL"
+  );
+});
+
+test('Adapter generates the correct signature', function(assert) {
+  var adapter = this.subject();
+  var pattern = /^\?ts=\d+\&apikey=.*$/;
+  assert.equal(
+    pattern.test(adapter.generateSignature()),
+    true,
+    "generateSignature should create the correct query params"
   );
 });

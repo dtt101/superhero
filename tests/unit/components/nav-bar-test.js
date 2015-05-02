63f886dafcb358ef0812bba6c4433c20592891e4
David Thompson <dtt101@gmail.com>
2015-05-02 12:51:05 +0100

    Test for links in nav bar component

--- tests/unit/components/nav-bar-test.js
+++ tests/unit/components/nav-bar-test.js
@@ -6,15 +6,14 @@ import {
 moduleForComponent('nav-bar', {});
 
 test('it renders', function(assert) {
-  assert.expect(2);
+  assert.expect(3);
 
-  // creates the component instance
   var component = this.subject();
   assert.equal(component._state, 'preRender');
 
-  // renders the component to the page
   this.render();
   assert.equal(component._state, 'inDOM');
 
-  
+  assert.equal(this.$().find('a').length, 2);
+
 });

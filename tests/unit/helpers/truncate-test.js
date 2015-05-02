0961b8350f71e19ff4313db3deca1797626bd094
David Thompson <dtt101@gmail.com>
2015-05-03 00:45:17 +0100

    Fixed truncate text helper test

--- tests/unit/helpers/truncate-test.js
+++ tests/unit/helpers/truncate-test.js
@@ -5,7 +5,7 @@ import { module, test } from 'qunit';
 
 module('TruncateTextHelper');
 
-test('it works', function(assert) {
-  var result = truncateText('this text should be limited', {limit: 5});
-  assert.ok(result);
+test('it truncates text to the specified limit', function(assert) {
+  var result = truncateText(['this text should be limited'], {limit: 5});
+  assert.equal(result, 'th...');
 });

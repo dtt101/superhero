f7f833d2b598fb451e8207d286cf8759331fb2eb
David Thompson <dtt101@gmail.com>
2015-05-03 21:48:22 +0100

    Remove reset scroll mixin

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,5 +1,5 @@
 import Ember from 'ember';
-import LoadingSliderMixin from '../mixins/loading-slider';
 
-export default Ember.Route.extend(LoadingSliderMixin, {
+export default Ember.Route.extend({
+  
 });
--- app/routes/characters/index.js
+++ app/routes/characters/index.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
-import ResetScroll from '../../mixins/reset-scroll';
 
-export default Ember.Route.extend(ResetScroll, {
+export default Ember.Route.extend({
     queryParams: {
       query: {
         refreshModel: true

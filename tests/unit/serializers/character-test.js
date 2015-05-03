fa6e242baba489f93378e0c26d658df481779a45
David Thompson <dtt101@gmail.com>
2015-05-03 22:35:30 +0100

    Added serialised test

--- tests/unit/serializers/character-test.js
+++ tests/unit/serializers/character-test.js
@@ -2,14 +2,24 @@ import {
   moduleFor,
   test
 } from 'ember-qunit';
+import data from '../../fixtures/characters';
 
 moduleFor('serializer:character', {
   // Specify the other units that are required for this test.
   // needs: ['serializer:foo']
 });
 
-// Replace this with your real tests.
-test('it exists', function(assert) {
+test('it serializes the character data', function(assert) {
+  assert.expect(1);
   var serializer = this.subject();
-  assert.ok(serializer);
+
+  var payload = JSON.stringify(data());
+  var store = null;
+  var type = null;
+  var id = null;
+  var requestType = null;
+  var serializedData = serializer.serialize(store, type, payload, id, requestType);
+
+  assert.equal(serializedData['charcters'].length, 20);
+
 });

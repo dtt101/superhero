6723194485a9acabd07270b9bf0aee84cdc45659
David Thompson <dtt101@gmail.com>
2015-05-24 10:06:28 +0100

    Ensure character images are served over https

--- app/transforms/thumbnail.js
+++ app/transforms/thumbnail.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Transform.extend({
   deserialize: function(serialized) {
     if (serialized) {
-      serialized = serialized.path + "." + serialized.extension;
+      serialized = serialized.path.replace('http://','https://') + "." + serialized.extension;
     }
     return serialized;
   },

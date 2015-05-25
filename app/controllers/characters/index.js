b661e62b1eb0163b6b8d6e7c40229090f9282b60
David Thompson <dtt101@gmail.com>
2015-05-25 18:37:06 +0100

    Only show next button if more results available

--- app/controllers/characters/index.js
+++ app/controllers/characters/index.js
@@ -12,13 +12,17 @@ export default Ember.ArrayController.extend({
     return this.get("content.meta");
   }.property('content.[]'),
 
+  resultsAvailable: Ember.computed('content.[]', function() {
+    let total = this.get('meta').total - this.get('increment');
+    return (this.get('offset') <= total);
+  }),
+
   actions: {
     search: function() {
       this.set('query', this.get('queryField'));
       this.set('offset', 0);
     },
     next: function() {
-      // TODO: check if there are any more results - compare offset to total
       var next = this.get('increment') + this.get('meta').offset;
       this.set('offset', next);
     }
